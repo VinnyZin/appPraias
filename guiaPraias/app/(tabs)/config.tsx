a66f3bffc68b9c19da43da33642b3b7c1e8e9165
VinnyZin <vinny151006@gmail.com>
2025-11-26 18:33:20 +0000

    Final

--- guiaPraias/app/(tabs)/config.tsx
+++ guiaPraias/app/(tabs)/config.tsx
@@ -82,7 +82,7 @@ export default function ConfigScreen() {
                 <View style={styles.modalOverlay}>
                     {/* Caixa de conteúdo do modal */}
                     <View style={styles.modalContent}>
-                        <Text style={styles.modalText}>Desenvolvido por: Aluno</Text>
+                        <Text style={styles.modalText}>Desenvolvido por: Vinicius Rocha</Text>
                         <TouchableOpacity
 
                             style={styles.modalCloseButton}
